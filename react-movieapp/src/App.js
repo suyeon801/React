f6c91518c3f2ccb874e5f8a13ae67f5a67beae32
suyeon801 <suyeon801@duksung.ac.kr>
2023-02-16 17:05:44 +0900

    add toDos

--- react-movieapp/src/App.js
+++ react-movieapp/src/App.js
@@ -27,6 +27,13 @@ function App() {
         ></input>
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {" "}
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
