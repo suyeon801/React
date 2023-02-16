2234fb55f304b5369c91ecb986202e82cd428934
suyeon801 <suyeon801@duksung.ac.kr>
2023-02-16 16:37:54 +0900

    add toDos

--- react-movieapp/src/App.js
+++ react-movieapp/src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
 
   const onChange = (event) => setToDo(event.target.value);
   const onSubmit = (event) => {
@@ -9,11 +10,14 @@ function App() {
     if (toDo === "") {
       return;
     }
+    setToDos((currentArray) => [toDo, ...currentArray]);
     setToDo("");
   };
+  console.log(toDos);
 
   return (
     <div>
+      <h1>My To Dos ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
         <input
           onChange={onChange}
