fff3d8ee35556bdc915ec221e13a5d2f3bbbfff4
suyeon801 <suyeon801@duksung.ac.kr>
2023-02-16 15:20:08 +0900

    add useEffect_Deps

--- react-movie/src/App.js
+++ react-movie/src/App.js
@@ -4,14 +4,29 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("I run all the time");
+
   useEffect(() => {
     console.log("CALL THE API,,,");
   }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("SEARCH FOR ", keyword);
+    }
+  }, [keyword]);
 
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1 className={styles.title}>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
