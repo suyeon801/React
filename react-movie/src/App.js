f90733919d52af25724b39166d16585985616d9b
suyeon801 <suyeon801@duksung.ac.kr>
2023-02-16 14:56:55 +0900

    add useeffect

--- react-movie/src/App.js
+++ react-movie/src/App.js
@@ -1,10 +1,14 @@
 import Button from "./Button";
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
+  console.log("I run all the time");
+  useEffect(() => {
+    console.log("CALL THE API,,,");
+  }, []);
 
   return (
     <div>
