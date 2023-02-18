a8dcadb678e0de88472b8504d7d67e6abc7039c4
suyeon <suyeon801@duksung.ac.kr>
2023-02-19 03:07:00 +0900

    add MovieDetail

--- react-movieapp/src/components/MovieDetail.js
+++ react-movieapp/src/components/MovieDetail.js
@@ -1,14 +1,12 @@
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
 
-function Movie({ id, coverImg, title, summary, genres }) {
+function MovieDetail({ coverImg, title, description_full, rating, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
-      <h2>
-        <Link to={`/movie/${id}`}>{title}</Link>
-      </h2>
-      <p>{summary}</p>
+      <h2>{title}</h2>
+      <p>{description_full}</p>
+      <p>{rating}</p>
       <ul>
         {genres.map((g) => (
           <li key={g}>{g}</li>
@@ -18,11 +16,12 @@ function Movie({ id, coverImg, title, summary, genres }) {
   );
 }
 
-Movie.protoTypes = {
-  id: PropTypes.number.isRequired,
+MovieDetail.protoTypes = {
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  summary: PropTypes.string.isRequired,
+  description_full: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
-export default Movie;
+
+export default MovieDetail;
--- react-movieapp/src/routes/Detail.js
+++ react-movieapp/src/routes/Detail.js
@@ -1,4 +1,44 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+//import { useCallback } from "react";
+import MovieDetail from "../components/MovieDetail";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
+
+  const { id } = useParams();
+
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+
+    setMovie(json.data.movie);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovie();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <MovieDetail
+            key={movie.id}
+            coverImg={movie.medium_cover_image}
+            title={movie.title}
+            description_full={movie.description_full}
+            rating={movie.rating}
+            genres={movie.genres}
+          />
+        </div>
+      )}
+    </div>
+  );
 }
 export default Detail;
